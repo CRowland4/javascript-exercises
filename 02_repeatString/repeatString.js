55d904a3c87079baa0afe5d7d4576c9b99579559
Caleb Rowland <cjrowland4@gmail.com>
2024-10-17 09:51:31 -0500

    Add solutions to exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
 
+    let solution = ""
+    for (let i = 0; i < num; i++) {
+        solution += string;
+    }
+    return solution;
 };
 
 // Do not edit below this line
