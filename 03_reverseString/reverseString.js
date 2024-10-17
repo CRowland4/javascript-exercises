dae406de07cadb3b68ab9c5e82011e9b08550bed
Caleb Rowland <cjrowland4@gmail.com>
2024-10-17 09:57:51 -0500

    Add solutions to exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let letters = [];
+    for (let i = 0; i < string.length; i++) {
+        letters.unshift(string[i]);
+    }
 
+    let solution = letters.join("");
+    return solution;
 };
 
 // Do not edit below this line
