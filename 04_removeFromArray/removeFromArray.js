fad49b9a23497d67249f00b271e2075b86c71430
Caleb Rowland <cjrowland4@gmail.com>
2024-10-17 20:10:46 -0500

    Add solutions to exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...items) {
+    let solution = [];
+    for (const element of array) {
+        if (!items.includes(element)) {
+            solution.push(element);
+        }
+    };
+
+    return solution;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
