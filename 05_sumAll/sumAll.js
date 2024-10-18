9bb3de5f7c3c2b40399bea0ac2c8bf7765eb38c0
Caleb Rowland <cjrowland4@gmail.com>
2024-10-17 20:39:14 -0500

    Add solutions to exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (!(Number.isInteger(num1)) || !(Number.isInteger(num2))) {
+        return "ERROR";
+    }
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    };
 
+
+    let solution = 0;
+    let smaller = num1;
+    let larger = num2;
+
+    if (num1 > num2) {
+        smaller = num2;
+        larger = num1;
+    };
+
+    for (let i = smaller; i <=  larger; i++) {
+        solution += i;
+    };
+    
+    return solution;
 };
 
 // Do not edit below this line
