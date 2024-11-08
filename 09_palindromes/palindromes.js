112c357945d31b5af978a5ee26337cf5ebaae7ed
Caleb Rowland <cjrowland4@gmail.com>
2024-11-08 07:24:39 -0600

    Add solution to problem 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function(word) {
+    const lettersAndNumbersArray = 'abcdefghijklmnopqrstuvwxyz1234567890'.split('');
+    let wordArray = word.toLowerCase().split("");
+    wordArray = wordArray.filter((char) => lettersAndNumbersArray.includes(char));
 
+    let reverseWordArray = wordArray.slice().reverse();
+    for (let i = 0; i < word.length; i++) {
+        if (wordArray[i] !== reverseWordArray[i]) {
+            return false;
+        }
+    }
+
+    return true
 };
 
 // Do not edit below this line
