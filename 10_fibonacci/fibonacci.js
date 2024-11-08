7a3b6c790761e646956034ac11e500c193b48a10
Caleb Rowland <cjrowland4@gmail.com>
2024-11-08 08:58:42 -0600

    Add solutions to exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num);
 
+    if (num === 0) {
+        return 0;
+    }
+    if (num < 0) {
+        return "OOPS";
+    }
+    let fibs = [1, 1];
+    if (num <= 1) {
+        return fibs[num];
+    }
+
+    while (fibs.length <= num) {
+        fibs.push(fibs[fibs.length - 1] + fibs[fibs.length - 2])
+        console.log(fibs);
+    }
+
+    return fibs[num - 1];
 };
 
 // Do not edit below this line
