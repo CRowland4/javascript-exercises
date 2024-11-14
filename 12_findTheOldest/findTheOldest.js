91d6ab4696676246e61514ba4225710daf10155f
Caleb Rowland <cjrowland4@gmail.com>
2024-11-14 09:21:08 -0600

    Add solutions to exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const date = new Date();
+    const currentYear = date.getFullYear();
 
+    let oldest = people[0];
+    oldestAge = getPersonAge(oldest, currentYear);
+
+    for (const person of people) {
+        const personAge = getPersonAge(person, currentYear);
+        if (personAge > oldestAge) {
+            oldest = person;
+            oldestAge = personAge;
+        }
+    }
+
+    return oldest;
 };
+
+
+function getPersonAge(person, currentYear) {
+    if ("yearOfDeath" in person) {
+        return person.yearOfDeath - person.yearOfBirth;
+    } else {
+        return currentYear - person.yearOfBirth;
+    }
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
